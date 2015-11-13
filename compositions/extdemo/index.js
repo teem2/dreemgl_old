a7f2db81b76f2994aed1d020b9968525431df850
gnovos <freemason@gmail.com>
2015-11-12 17:00:57 -0800

    Adding license header to many files

--- compositions/extdemo/index.js
+++ compositions/extdemo/index.js
@@ -1,3 +1,8 @@
+/* Copyright 2015 Teem2 LLC - Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ either express or implied. See the License for the specific language governing permissions and limitations under the License.*/
+
 define.class(function(composition, require, screens, desktop, devices, guide$search, syntax, this$index){
 
 	function getSource(obj) {
@@ -8,7 +13,7 @@ define.class(function(composition, require, screens, desktop, devices, guide$sea
 
 		return [
 			// `compositions/guide/search.js` is used here
-			guide$search({name:'search', keyword:"Cats"}),
+			guide$search({name:'search', keyword:"Aliens"}),
 			screens(
 				desktop({
 					name:'desktop',
