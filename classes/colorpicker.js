1b129803ec567f25ef52f6c94cf5bc5e8f66f912
Stijn Kuipers <stijnkuipers@gmail.com>
2015-11-26 03:56:43 +0100

    color ring - color lib for shaders - colorpicker progress

--- classes/colorpicker.js
+++ classes/colorpicker.js
@@ -4,8 +4,12 @@
    either express or implied. See the License for the specific language governing permissions and limitations under the License.*/
 
 
-define.class(function(view, label,button, scrollbar){
+define.class(function(view, label,button, scrollbar,require){
 	
+	
+	var Shader = this.Shader = require('$draw/$drawmode/shader$drawmode')
+
+		
 	this.attributes =  {
 		color: {type: vec4, value: "white"}
 		,fgcolor: {type: vec4, value: "white"}		
@@ -21,6 +25,53 @@ define.class(function(view, label,button, scrollbar){
 	this.bordercolor = this.internalbordercolor
 	this.internalbordercolor= function(){
 		this.bordercolor = this.internalbordercolor		
+	}
+	define.class(this, 'colorcircle', this.Shader, function(){
+		this.vertexstruct = define.struct({
+			
+			p:float,
+			side: float
+		})
+
+		this.mesh = this.vertexstruct.array()
+			this.draw_type = "TRIANGLE_STRIP"
+	
+		this.position = function(){
+			uv = vec2(sin(mesh.p), cos(mesh.p))*mesh.side;
+			off = mesh.p / 6.283
+			var rad = min(view.layout.width, view.layout.height)/2;
+			pos = vec2(view.layout.width/2 + rad * uv.x, view.layout.height/2 + rad * uv.y)
+			return vec4(pos, 0, 1) * view.totalmatrix * view.viewmatrix
+		}
+		
+		this.color = function(){
+			return colorlib.hsla(vec4(off, 1, 0.5, 1));
+		}
+		
+		this.update = function(){
+			var view = this.view
+			var width = view.layout?view.layout.width:view.width
+			var height = view.layout?view.layout.height:view.height
+			var cx = width/2;
+			var cy = height/2;
+			var radius = Math.min(cx,cy);
+			this.mesh = this.vertexstruct.array()
+			var cnt = 200;
+			for (var i = 0;i<cnt;i++)
+			{
+				this.mesh.push(i*6.283/(cnt-1), 0.7);
+				this.mesh.push(i*6.283/(cnt-1), 1);
+			}
+		}
+			
+		
+	
+	})
+	
+	this.color = function(aaaa){
+		console.log(aaaa);
+		
+		this.settingcontrol = undefined;
 	}
 	
 	define.class(this, 'colorarea', function(view){
@@ -38,6 +89,7 @@ define.class(function(view, label,button, scrollbar){
 		return [
 			view({flexdirection:"row", flex:1, bgcolor:"transparent"}
 				,this.colorarea()
+				,view({bg:this.colorcircle(), width:100,height:100})
 				,view({flexdirection:"column", flex:1,bgcolor:"transparent"}
 					,view({bgcolor:"transparent", flexdirection:"row" }
 						,label({fgcolor:this.fgcolor, bgcolor:"transparent" , text:"R", fontsize:14, margin:4})			
@@ -67,7 +119,23 @@ define.class(function(view, label,button, scrollbar){
 						,label({fgcolor:this.fgcolor, bgcolor:"transparent" , text:"H", fontsize:14, margin:4})			
 						,view({bgcolor:"transparent", flexdirection:"column", flex:1 }
 							,scrollbar({height:18, total:255, page:20, vertical:false})
-							,view({height:10, bg:{color:function(){return vec4(0,0,uv.x,1);}}})
+							,view({height:10, bg:{color:function(){return colorlib.hsla(vec4(uv.x,1,0.5,1));}}})
+						)
+						,label({fgcolor:this.fgcolor, bgcolor:"transparent" , text:"255", fontsize:14, margin:4})			
+					)			
+					,view({bgcolor:"transparent", flexdirection:"row" }
+						,label({fgcolor:this.fgcolor, bgcolor:"transparent" , text:"H", fontsize:14, margin:4})			
+						,view({bgcolor:"transparent", flexdirection:"column", flex:1 }
+							,scrollbar({height:18, total:255, page:20, vertical:false})
+							,view({height:10, bg:{color:function(){return colorlib.hsla(vec4(0,1,uv.x,1));}}})
+						)
+						,label({fgcolor:this.fgcolor, bgcolor:"transparent" , text:"255", fontsize:14, margin:4})			
+					)			
+					,view({bgcolor:"transparent", flexdirection:"row" }
+						,label({fgcolor:this.fgcolor, bgcolor:"transparent" , text:"H", fontsize:14, margin:4})			
+						,view({bgcolor:"transparent", flexdirection:"column", flex:1 }
+							,scrollbar({height:18, total:255, page:20, vertical:false})
+							,view({height:10, bg:{color:function(){return colorlib.hsla(vec4(0,uv.x,0.5,1));}}})
 						)
 						,label({fgcolor:this.fgcolor, bgcolor:"transparent" , text:"255", fontsize:14, margin:4})			
 					)			
